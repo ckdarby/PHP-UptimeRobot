dd41808b0e336ea056af0201138ad0b97712b9fe
ckdarby <ckdarby@gmail.com>
2013-10-20 19:50:08 +0000

    API & Doc for UptimeRobot

--- README.md
+++ README.md
@@ -1,4 +1,58 @@
--PHP-UptimeRobot
-================
+PHP Wrapper For UptimeRobot.com
+==============
 
-PHP wrapper for UptimeRobot.com API
+This is a basic PHP wrapper for https://uptimerobot.com/api
+
+## Prerequisites
+* Configure the $config apiKey
+* Must be running PHP >= 5.4
+
+##Composer
+Add this to your composer.json
+
+```JSON
+{
+    "require": {
+        "ckdarby/php-uptimerobot": "dev-master"
+    }
+}
+```
+
+## Example
+
+```PHP
+<?php
+//Requires composer install to work
+require_once(__DIR__.'/vendor/autoload.php');
+
+use UptimeRobot\API;
+
+//Set configuration settings
+$config = [
+    'apiKey' => 'APIKEY',
+    'url' => 'http://api.uptimerobot.com'
+];
+
+try {
+
+    //Initalizes API with config options
+    $api = new API($config);
+
+    //Define parameters for our getMethod request
+    $args = [
+        'showTimezone' => 1
+    ];
+
+    //Makes request to the getMonitor Method
+    $results = $api->request('/getMonitors', $args);
+
+    //Output json_decoded contents
+    var_dump($results);
+
+} catch (Exception $e) {
+    echo $e->getMessage();
+    //Output various debug information
+    var_dump($api->debug);
+}
+
+```
--- src/UptimeRobot/API.php
+++ src/UptimeRobot/API.php
@@ -5,4 +5,96 @@ namespace UptimeRobot;
 class API
 {
 
+    private $url, $contents;
+    public $debug;
+
+    /**
+     * Initializes the API
+     *
+     * @param  array  $config  An array of configuration
+     * @throws Exception Configuration is missing
+     */
+    public function __construct($config = array()) {
+
+        if(empty($config['apiKey'])) {
+            throw new \Exception('Missing API Key');
+        }
+
+        if(empty($config['url'])) {
+            throw new \Exception('Missing API url');
+        }
+
+        //Setting apiKey, Format & noJsonCallBack
+        $this->args['apiKey'] = $config['apiKey'];
+        $this->args['format'] = 'json';
+        $this->args['noJsonCallback'] = 1;
+
+        $this->url = $config['url'];
+    }
+
+    /**
+     * Makes curl call to the url & returns output
+     *
+     * @param  string  $resource  The resource of the api
+     * @param  array  $args  Array of options for the query query
+     * @throws Exception If the curl request fails
+     * @return  array  Finalized Url
+     */
+    public function request($resource, $args=array())
+    {
+
+        $url = $this->buildUrl($resource, $args);
+        $curl = curl_init($url);
+
+        $options = array(
+            CURLOPT_HEADER         => false,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => false,
+        );
+
+        curl_setopt_array($curl, $options);
+        $this->contents = curl_exec($curl);
+        $this->setDebug($curl);
+
+        if(curl_errno($curl) > 0) {
+            throw new \Exception('There was an error while making the request');
+        }
+
+        return json_decode($this->contents, true);
+    }
+
+    /**
+     * Builds the url for the curl request
+     *
+     * @param  string  $resource  The resource of the api
+     * @param  array  $args  Array of options for the query query
+     * @return  string  Finalized Url
+     */
+    private function buildUrl($resource, $args)
+    {
+        //Merge args(apiKey, Format, noJsonCallback)
+        $args = array_merge($args, $this->args);
+        $query = http_build_query($args);
+
+        $url = $this->url;
+        $url .= $resource.'?'.$query;
+
+        return $url;
+    }
+
+    /**
+     * Sets debug information from last curl
+     *
+     * @param  resource  $curl  Curl handle
+     */
+    private function setDebug($curl)
+    {
+        $this->debug = [
+            'errorNum' => curl_errno($curl),
+            'error' => curl_error($curl),
+            'info' => curl_getinfo($curl),
+            'raw' => $this->contents,
+        ];
+
+    }
 }
