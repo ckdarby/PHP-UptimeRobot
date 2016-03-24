5aee95c39a90a327ad9621140daf9dc11e0baa7d
nqxcode <nc101ux@gmail.com>
2016-03-24 18:47:28 +0300

    Added ability to set options for curl in constructor.

--- src/UptimeRobot/API.php
+++ src/UptimeRobot/API.php
@@ -1,62 +1,68 @@
-<?php
+<?php namespace UptimeRobot;
 
-namespace UptimeRobot;
-
+/**
+ * Class API
+ * @package UptimeRobot
+ */
 class API
 {
+    private $url;
+    private $contents;
 
-    private $url, $contents;
+    private $args;
+    private $options;
+
     public $debug;
 
     /**
-     * Initializes the API
+     * Initializes the API.
      *
-     * @param  array  $config  An array of configuration
-     * @throws Exception Configuration is missing
+     * @param array $config An array of configuration
+     * @param array $options An array of options for curl
+     *
+     * @throws \Exception Configuration is missing
      */
-    public function __construct($config = array()) {
-
-        if(empty($config['apiKey'])) {
+    public function __construct($config = array(), $options = array())
+    {
+        if (empty($config['apiKey'])) {
             throw new \Exception('Missing API Key');
         }
 
-        if(empty($config['url'])) {
+        if (empty($config['url'])) {
             throw new \Exception('Missing API url');
         }
 
-        //Setting apiKey, Format & noJsonCallBack
+        // Setting apiKey, Format & noJsonCallBack
         $this->args['apiKey'] = $config['apiKey'];
         $this->args['format'] = 'json';
         $this->args['noJsonCallback'] = 1;
 
         $this->url = $config['url'];
+
+        // Set options for curl
+        $this->options = $this->getOptions($options);
     }
 
     /**
-     * Makes curl call to the url & returns output
+     * Makes curl call to the url & returns output.
      *
-     * @param  string  $resource  The resource of the api
-     * @param  array  $args  Array of options for the query query
-     * @throws Exception If the curl request fails
-     * @return  array  json_decoded contents
+     * @param string $resource The resource of the api
+     * @param array $args Array of options for the query query
+     *
+     * @return array json_decoded contents
+     * @throws \Exception If the curl request fails
      */
-    public function request($resource, $args=array())
+    public function request($resource, $args = array())
     {
 
         $url = $this->buildUrl($resource, $args);
         $curl = curl_init($url);
 
-        $options = array(
-            CURLOPT_HEADER         => false,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_SSL_VERIFYPEER => false,
-        );
-
-        curl_setopt_array($curl, $options);
+        curl_setopt_array($curl, $this->options);
         $this->contents = curl_exec($curl);
         $this->setDebug($curl);
 
-        if(curl_errno($curl) > 0) {
+        if (curl_errno($curl) > 0) {
             throw new \Exception('There was an error while making the request');
         }
 
@@ -64,11 +70,37 @@ class API
     }
 
     /**
-     * Builds the url for the curl request
+     * Get options for curl.
      *
-     * @param  string  $resource  The resource of the api
-     * @param  array  $args  Array of options for the query query
-     * @return  string  Finalized Url
+     * @param $options
+     *
+     * @return array
+     */
+    private function getOptions($options)
+    {
+        $conf = [
+            CURLOPT_HEADER => false,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => false,
+        ];
+
+        if (isset($options['timeout'])) {
+            $conf[CURLOPT_TIMEOUT_MS] = $options['timeout'] * 1000;
+        }
+        if (isset($options['connect_timeout'])) {
+            $conf[CURLOPT_CONNECTTIMEOUT_MS] = $options['connect_timeout'] * 1000;
+        }
+
+        return $conf;
+    }
+
+    /**
+     * Builds the url for the curl request.
+     *
+     * @param string $resource The resource of the api
+     * @param array $args Array of options for the query query
+     *
+     * @return string  Finalized Url
      */
     private function buildUrl($resource, $args)
     {
@@ -77,15 +109,15 @@ class API
         $query = http_build_query($args);
 
         $url = $this->url;
-        $url .= $resource.'?'.$query;
+        $url .= $resource . '?' . $query;
 
         return $url;
     }
 
     /**
-     * Sets debug information from last curl
+     * Sets debug information from last curl.
      *
-     * @param  resource  $curl  Curl handle
+     * @param resource $curl Curl handle
      */
     private function setDebug($curl)
     {
@@ -95,6 +127,5 @@ class API
             'info' => curl_getinfo($curl),
             'raw' => $this->contents,
         ];
-
     }
 }
